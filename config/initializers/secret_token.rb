6d2b4055ac2898a044cc614d2c790168e9196d8a
MATC Student <student@PHPubuntu.(none)>
2011-01-25 19:52:54 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2cb239f589f3ec2556eda7dade3d227dfec5c5dce1d48c002627f12d835f9e61db086ea267e1917253af511398a3ffb0206712ed420af95c3e154f58cff6dc92'
